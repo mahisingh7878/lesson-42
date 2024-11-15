9bd0c040599bc8cb542182d0049c2217830502a6
mahisingh4415 <mahisinghadityasingh1232@gmail.com>
2024-11-15 21:24:41 +0530

    actvitya1

---
+++ actvity1.py
@@ -0,0 +1,20 @@
+import turtle 
+
+turtle.Screen().bgcolor("red")
+
+sc=turtle.Screen()
+sc.setup(500,400)
+
+turtle.title("actvity1")
+
+t1=turtle.Turtle()
+
+for i in range(4):
+    t1.forward(100)
+    t1.left(90)
+    i=i+1
+
+
+
+turtle.done()
+
